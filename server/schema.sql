d01a353c54d99a276c0d08b36d50a6f487062471
selfresonator <selfresonator@gmail.com>
2016-04-08 19:57:09 -0700

    Database mostly created

--- server/schema.sql
+++ server/schema.sql
@@ -3,15 +3,19 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-
+  text VARCHAR(140),
+  user_id INT UNSIGNED NOT NULL,
+  room_id INT UNSIGNED NOT NULL,
+  message_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
 CREATE TABLE users (
+  user_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
 
 );
 
 CREATE TABLE rooms (
-
+  room_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 /* Create other tables and define schemas for them here! */
 
